ddd287bc584e1183f4439df7d353a55006be760a
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2019-08-18 18:55:18 +0300

    chuncked router

--- src/main/java/com/dbbyte/App.java
+++ src/main/java/com/dbbyte/App.java
@@ -3,6 +3,7 @@ package com.dbbyte;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
 import sun.security.provider.certpath.Vertex;
 
@@ -30,8 +31,39 @@ public class App
             response.end("message back to the caller");
         });
 
+        router.route("/hello").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.putHeader("content-type","text/plain");
+            response.end("message back for hello path");
+        });
+
+        // chunks for multiple handlers
+        Route handler1 = router.route("/chunked").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.setChunked(true);
+            response.write("First Chunked of data\n");
+
+            routingContext.vertx().setTimer(5000, tid -> {
+                routingContext.next();
+            });
+        });
+        Route handler2 = router.route("/chunked").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.write("Second Chunked of data\n");
+
+            routingContext.vertx().setTimer(10000, tid -> {
+                routingContext.next();
+            });
+        });
+        Route handler3 = router.route("/chunked").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.end("End Response");
+        });
+
+
+
         // set the port to listen to
-        httpServer.requestHandler(router).listen(8080);
+        httpServer.requestHandler(router).listen(9090);
 
     }
 }
